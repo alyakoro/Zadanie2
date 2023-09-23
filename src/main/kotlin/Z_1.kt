376c70c539dee1927cad0f75216b7451a8138fa7
1101-22 <alya.korobeynikova99@gmail.com>
2023-09-23 16:21:23 +0300

    ы

--- src/main/kotlin/Z_1.kt
+++ src/main/kotlin/Z_1.kt
@@ -1,7 +1,12 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    println("Введите количество строк: ")
+    val x = readln().toInt()
+    println("Введите количество столбцов: ")
+    val y = readln().toInt()
+    val arr = Array(x) {
+        IntArray(y)
+    }
+    for (row in arr){
+        println(row.contentToString())
+    }
 }
